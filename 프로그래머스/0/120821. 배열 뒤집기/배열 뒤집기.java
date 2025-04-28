59d6c33931357306a5711fee378328ceaa89b9bc
Mina Ok <93837213+okaymina@users.noreply.github.com>
2025-04-28 11:12:18 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,18 +1,9 @@
-import java.util.*;
 class Solution {
-    public int[] solution(int[] num_list) {
-        int[] answer = {};
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i : num_list){
-            list.add(i);
+        public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        for(int i = 0; i< num_list.length; i++){
+            answer[i] = num_list[num_list.length-i-1];
         }
-        //배열 뒤집기
-        Collections.reverse(list);
-        //출력 테스트
-        System.out.println(list);
-        //Integer배열을 int배열로 형변환
-        answer = list.stream().mapToInt(i -> i).toArray();   
-
         return answer;
     }
 }
